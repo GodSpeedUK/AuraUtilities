3e10cbea2001b3d69a4b96231f64e9db435708db
Dan <dan@aurasoftware.tech>
2024-04-04 00:26:12 +0100

    Fixed GUI Bugs

--- src/main/java/tech/aurasoftware/aurautilities/gui/AuraGUI.java
+++ src/main/java/tech/aurasoftware/aurautilities/gui/AuraGUI.java
@@ -2,6 +2,7 @@ package tech.aurasoftware.aurautilities.gui;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.bukkit.Bukkit;
 import org.bukkit.inventory.Inventory;
 import org.jetbrains.annotations.NotNull;
@@ -10,8 +11,8 @@ import tech.aurasoftware.aurautilities.util.Text;
 
 import java.util.List;
 
-@Builder
 @Getter
+@NoArgsConstructor
 public class AuraGUI implements Serializable, Cloneable {
 
     private String id;
@@ -19,14 +20,35 @@ public class AuraGUI implements Serializable, Cloneable {
     private int size;
     private List<AuraGUIItem> items;
 
+    public AuraGUI id(String id){
+        this.id = id;
+        return this;
+    }
+
+    public AuraGUI name(String name){
+        this.name = name;
+        return this;
+    }
+
+    public AuraGUI size(int size){
+        this.size = size;
+        return this;
+    }
+
+    public AuraGUI items(List<AuraGUIItem> items){
+        this.items = items;
+        return this;
+    }
+
+
 
     @Override
     public AuraGUI clone() {
-        return AuraGUI.builder()
+        return new AuraGUI()
+                .id(id)
                 .name(name)
                 .size(size)
-                .items(items)
-                .build();
+                .items(items);
     }
 
     public Inventory createInventory(AuraGUIItem... additionalItems){
--- src/main/java/tech/aurasoftware/aurautilities/gui/AuraGUIItem.java
+++ src/main/java/tech/aurasoftware/aurautilities/gui/AuraGUIItem.java
@@ -2,15 +2,31 @@ package tech.aurasoftware.aurautilities.gui;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import tech.aurasoftware.aurautilities.configuration.serialization.Serializable;
 import tech.aurasoftware.aurautilities.item.AuraItem;
 
-@Builder
+@NoArgsConstructor
 @Getter
 public class AuraGUIItem implements Serializable {
 
     private AuraItem auraItem;
     private int slot;
     private String key;
+
+    public AuraGUIItem auraItem(AuraItem auraItem){
+        this.auraItem = auraItem;
+        return this;
+    }
+
+    public AuraGUIItem slot(int slot){
+        this.slot = slot;
+        return this;
+    }
+
+    public AuraGUIItem key(String key){
+        this.key = key;
+        return this;
+    }
 
 }
